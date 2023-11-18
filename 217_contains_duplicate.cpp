d2dbf42b011fe8b23a689728e6e2a8335766a427
Tyson Franks <tjfranks02@hotmail.com>
2023-11-18 13:33:10 +1100

    Started again in c++!

--- 217_contains_duplicate.cpp
+++ 217_contains_duplicate.cpp
@@ -1,27 +1,21 @@
 /*
-* Uses a set. Loops through each element in nums and inserts it into the set.
-* If element is already in the set, return true. False if we get to the end.
-*
-* Time: O(n)
-* Memory: O(n)
+Task: Return whether the given vector contains duplicate elements or not
+
+Solution: Use a set to keep track of the elements seen so far. If we see an element that is already 
+in the set, return true. Otherwise, return false.
 */
-#include <set>
-#include <vector>
-
-using namespace std;
-
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
         set<int> seen;
-        
-        for (int i = 0; i < nums.size(); i++) {
-            if (seen.find(nums[i]) != seen.end()) {
+
+        for (int num : nums) {
+            if (seen.contains(num)) {
                 return true;
-            } else {
-                seen.insert(nums[i]);
             }
+            seen.insert(num);
         }
+        
         return false;
     }
 };
