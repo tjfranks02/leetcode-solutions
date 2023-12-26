ecfaaa486fc045e59767c6a57a5ac916478d4cc4
Tyson Franks <tjfranks02@hotmail.com>
2023-12-26 12:54:26 +1000

    Solved 91_decode_ways after looking at the neetcode solution

---
+++ 91_decode_ways.cpp
@@ -0,0 +1,33 @@
+/**
+ * Decode ways. Had to look at the solution for this one.
+ * 
+ * Kinda had the right idea but couldn't get one that generalises to all cases.
+ */
+class Solution {
+public:
+    int numDecodings(string s) {
+        if (s[0] == '0') {
+            return 0;
+        }
+
+        int n = s.size();
+        vector<int> dp(n + 1);
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            int ones = stoi(s.substr(i - 1, 1));
+            int tens = stoi(s.substr(i - 2, 2));
+
+            if (ones >= 1 && ones <= 9) {
+                dp[i] += dp[i - 1];
+            }
+
+            if (tens >= 10 && tens <= 26) {
+                dp[i] += dp[i - 2];
+            }
+        }
+    
+        return dp[n];
+    }
+};
